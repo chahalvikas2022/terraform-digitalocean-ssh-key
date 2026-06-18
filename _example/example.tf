50ac9b394021fd2c88e066d934d9b87f4c721294
chahalvikas2022 <chahalvikash2022@gmail.com>
2026-06-18 12:29:48 +0530

    inital commit

---
+++ _example/example.tf
@@ -0,0 +1,12 @@
+provider "digitalocean" {
+
+}
+#-------------------------------##########-----------------------------------------------------
+#module :This module for ssh-key
+#-------------------------------##########-----------------------------------------------------
+
+module "ssh-key" {
+  source     = "./../"
+  name       = "test"
+  public_key = "ssh-rsa AAAABo4lNDjjUQQh4c2bGLi0L3/CDOHCFeBJaxUWC9yo18LgIv+m4YpSsRIWu014keIrJO4O+vyoybTCVSLl9kWOs8wSXjrg1zqg5VqE/w5XqI+C3Wcrf4aTDJ17oFm1UCVmtpUbHNvd3DoXInozkxk6FRAQGWz4Nmanoj"
+}
